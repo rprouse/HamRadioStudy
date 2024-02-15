009b911fafa1da0eaae68ee97e5d9ff7414c6545
Rob Prouse <rob@prouse.org>
2024-02-14 20:20:13 -0500

    Move the questions to an embedded resource

--- ConvertTestQuestions/Program.cs
+++ ConvertTestQuestions/Program.cs
@@ -5,16 +5,27 @@ using HamRadioStudy.Entities;
 bool isEnglish = true;
 int offset = isEnglish ? 0 : 5;
 
-File.ReadAllLines("data/amat_basic_quest_delim.txt")
+// Load basic_questions.txt into a list of questions from a resource file
+var assembly = typeof(Program).Assembly;
+var resourceStream = assembly.GetManifestResourceStream("HamRadioStudy.Resources.basic_questions.txt");
+if (resourceStream == null)
+{
+    Console.WriteLine("Resource not found");
+    return;
+}
+// var questions = new List<Question>();
+
+using var reader = new StreamReader(resourceStream);
+reader.ReadToEnd()
+    .Split(new [] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries)
     .Skip(1)
     .Select(line => line.Split(';'))
-    .Select(parts =>
-        new Question
-        {
-            QuestionId = parts[0 + offset],
-            QuestionText = parts[1 + offset],
-            CorrectAnswer = parts[2 + offset],
-            IncorrectAnswers = parts.Skip(3 + offset).Take(3).ToList()
-        })
+    .Select(parts => new Question
+    {
+        QuestionId = parts[0 + offset],
+        QuestionText = parts[1 + offset],
+        CorrectAnswer = parts[2 + offset],
+        IncorrectAnswers = parts.Skip(3 + offset).Take(3).ToList()
+    })
     .ToList()
     .ForEach(Console.WriteLine);
